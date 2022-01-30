efac6ac3584827f2a1d7ec7a9b239e4ee11dfc82
mehmet yıldırım <mehmetydm@gmail.com>
2022-01-31 00:31:57 +0300

    text classification model..

--- NeuralNetwork/tutorial4-textClassification-model.py
+++ NeuralNetwork/tutorial4-textClassification-model.py
@@ -6,8 +6,6 @@ data = keras.datasets.imdb
 
 (train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
 
-print(train_data[0])
-
 word_index = data.get_word_index()
 word_index = {k:(v+3) for k, v in word_index.items()}
 word_index["<PAD>"] = 0
@@ -18,15 +16,18 @@ word_index["<UNUSED>"] = 3
 reverse_word_index = dict([value, key] for (key, value) in word_index.items())
 
 # preproccess data...
-train_data = keras.preproccessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=250)
-test_data = keras.preproccessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
-
-
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
 
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 
-print(decode_review(test_data[0]))
+# model burada..
 
+model = keras.Sequential()
+model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation="relu"))
+model.add(keras.layers.Dense(1, activation="sigmoid"))
 
-print(len(test_data[0]), len(test_data[1]))
+model.summary()
