e5725314cd210ce54326e2fcb982abaa69407354
mehmet yıldırım <mehmetydm@gmail.com>
2022-02-27 08:52:15 +0300

    dosya çıkarıldı

--- dropouts-experimental/tensorEnv/FakeImageVideoDedector/app.py
+++ dropouts-experimental/tensorEnv/FakeImageVideoDedector/app.py
@@ -19,7 +19,7 @@ detector = MTCNN()
 
 
 def deepfakespredict(input_img):
-    labels = ['real', 'fake']
+    labels = ['gerçek', 'sahte']
     pred = [0, 0]
     text = ""
     text2 = ""
@@ -40,51 +40,30 @@ def deepfakespredict(input_img):
         pred = model.predict(np.expand_dims(face_image4, axis=0))[0]
 
         if pred[1] >= 0.6:
-            text = "The image is FAKE."
+            text = "Bu resim SAHTE."
         elif pred[0] >= 0.6:
-            text = "The image is REAL."
+            text = "Bu resim GERÇEK."
         else:
-            text = "The image may be REAL or FAKE."
+            text = "Bu resim GERÇEK ya da SAHTE olabilir."
 
     else:
-        text = "Face is not detected in the image."
+        text = "Resimin içinde yüz ifadesi bulunmadı."
 
-    text2 = "REAL: " + str(np.round(pred[0] * 100, 2)) + "%, FAKE: " + str(np.round(pred[1] * 100, 2)) + "%"
+    text2 = "GERÇEK: " + str(np.round(pred[0] * 100, 2)) + "%, SAHTE: " + str(np.round(pred[1] * 100, 2)) + "%"
 
     return input_img, text, text2, {labels[i]: float(pred[i]) for i in range(2)}
 
 
-title = "EfficientNetV2 Deepfakes Image Detector"
-description = "This is a demo implementation of EfficientNetV2 Deepfakes Image Detector. \
-            To use it, simply upload your image, or click one of the examples to load them.  \
-            This demo and model represent the Final Year Project titled \"Achieving Face Swapped Deepfakes Detection Using EfficientNetV2\" by a CS undergraduate Lee Sheng Yeh.  \
-            The examples were extracted from Celeb-DF(V2)(Li et al, 2020) and FaceForensics++(Rossler et al., 2019). Full reference details is available in \"references.txt.\" \
-            The examples are used under fair use to demo the working of the model only. If any copyright is infringed, please contact the researcher via this email: tp054565@mail.apu.edu.my, the researcher will immediately take down the examples used.\
-            "
-
-examples = [
-    ['Fake-1.png'],
-    ['Fake-2.png'],
-    ['Fake-3.png'],
-    ['Fake-4.png'],
-    ['Fake-5.png'],
-
-    ['Real-1.png'],
-    ['Real-2.png'],
-    ['Real-3.png'],
-    ['Real-4.png'],
-    ['Real-5.png']
-
-]
+title = "Sahte Resim Dedektörü"
+description = "Resimlerin sahte olup olmadığını kontrol edebilirsiniz.  "
 
 gr.Interface(deepfakespredict,
-             inputs=["image"],
-             outputs=[gr.outputs.Image(type="pil", label="Detected face"),
+             inputs=[gr.inputs.Image(type="pil")],
+             outputs=[gr.outputs.Image(type="pil", label="Tespit Edilen Yüz"),
                       "text",
                       "text",
-                      gr.outputs.Label(num_top_classes=None, type="auto", label="Confidence")],
+                      gr.outputs.Label(num_top_classes=None, type="auto", label="Eminlik")],
              title=title,
              description=description,
-             examples=examples,
-             examples_per_page=5
+             theme="huggingface"
              ).launch()
